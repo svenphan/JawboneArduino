c62f25355b8247c9bbc1218b959ee810ce719caa
Steven Phan <steven.phan@SFM20TDF91L.local>
2014-11-21 11:12:01 -0800

    second commit

--- public/js/parseing.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $("button").click(function(){
-    $.getJSON("jawbone.js",function(result){
-      $.each(result, function(i, field){
-        $("div").append(field + " ");
-      });
-    });
-  });
-});
